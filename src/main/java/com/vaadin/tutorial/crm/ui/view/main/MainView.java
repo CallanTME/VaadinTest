e4c1cce09b328fae072bb7888683d9fd62b69055
CallanTME <callan.egan18@imperial.ac.uk>
2020-12-08 16:27:43 +0000

    Initial commit

---
+++ src/main/java/com/vaadin/tutorial/crm/ui/view/main/MainView.java
@@ -0,0 +1,49 @@
+package com.vaadin.tutorial.crm.ui.view.main;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+@PageTitle("Ward X")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        HorizontalLayout h1 = new HorizontalLayout();
+        add(new H1("Ward 1"), h1);
+        h1.setWidth("100%");
+        h1.setHeight("220px");
+        PatientLayout p1 = new PatientLayout("Olly","4","1");
+        PatientLayout p2 = new PatientLayout("Callan","6","2");
+        PatientLayout p3  = new PatientLayout("Nikita","5","3");
+        PatientLayout p4 = new PatientLayout("Alice","3","4");
+        h1.add(p1.getVerticalLayout());
+        h1.add(p2.getVerticalLayout());
+        h1.add(p3.getVerticalLayout());
+        h1.add(p4.getVerticalLayout());
+
+        HorizontalLayout h2 = new HorizontalLayout();
+        h2.setWidth("100%");
+        h2.setHeight("22  0px");
+        PatientLayout p5 = new PatientLayout("John","13","5");
+        PatientLayout p6 = new PatientLayout("Sean","9","6");
+        PatientLayout p7  = new PatientLayout("Charlie","23","7");
+        PatientLayout p8 = new PatientLayout("Rowan","17","8");
+        h2.add(p5.getVerticalLayout());
+        h2.add(p6.getVerticalLayout());
+        h2.add(p7.getVerticalLayout());
+        h2.add(p8.getVerticalLayout());
+
+        add(h1);
+        add(h2);
+
+    }
+}
---
+++ src/main/java/com/vaadin/tutorial/crm/ui/view/main/PatientLayout.java
@@ -0,0 +1,46 @@
+package com.vaadin.tutorial.crm.ui.view.main;
+
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.component.progressbar.ProgressBarVariant;
+
+import java.util.Random;
+
+public class PatientLayout {
+    private VerticalLayout layout_1;
+    //private ProgressBar progressBar;
+    //private Label name;
+    //private Label bScore;
+
+    public PatientLayout(String nameIn, String bScoreIn, String bedNumIn){
+        layout_1 = new VerticalLayout();
+        layout_1.setWidth("50%");
+        layout_1.setHeight("220px");
+        layout_1.getStyle().set("border", "1px solid #9E9E9E");
+        Random r = new Random();
+        int prog = r.nextInt(100);
+        ProgressBar progressBar = new ProgressBar(0, 100, prog);
+        progressBar.setHeight("20px");
+        if (prog < 33)
+            progressBar.addThemeVariants(ProgressBarVariant.LUMO_SUCCESS);
+        else if (prog > 33 & prog < 67)
+            progressBar.addThemeVariants(ProgressBarVariant.LUMO_CONTRAST);
+        else
+            progressBar.addThemeVariants(ProgressBarVariant.LUMO_ERROR);
+
+        Label bedNum = new Label("Bed Number: " + bedNumIn);
+        Label name = new Label("Name: " + nameIn);
+        Label bScore = new Label("Braden Score: " + bScoreIn);
+        Label time = new Label("Time in Position: 0h 0m 0s");
+        layout_1.add(bedNum,name,bScore,progressBar,time);
+
+        layout_1.setSpacing(true);
+        layout_1.setMargin(true);
+        layout_1.setPadding(true);
+    }
+
+    public VerticalLayout getVerticalLayout(){
+        return layout_1;
+    }
+}
